d0b831efe3e7b9509d6bdc82b7aef55e5859102b
GabrielaD <david.gabriela.99@gmail.com>
2022-03-21 18:21:40 +0200

    Styled flexbox items

--- ice-cream-shop/src/app/flavours/mock-flavours.ts
+++ ice-cream-shop/src/app/flavours/mock-flavours.ts
@@ -1,10 +1,10 @@
 import { Flavour } from './flavour';
 
 export const FLAVOURS: Flavour[] = [
-  { id: 11, name: 'Classic Banana', description: 'The raw taste of banana', img_url: "https://i.imgur.com/ycNLJ3E.png" },
-  { id: 12, name: 'Chocolate Banana', description: '', img_url: "https://i.imgur.com/CICksMW.png" },
-  { id: 13, name: 'Mango Banana', description: '', img_url: "https://i.imgur.com/tJeyf3J.png" },
-  { id: 14, name: 'Strawberry Banana', description: '', img_url: "https://i.imgur.com/yRKMSqe.png" },
-  { id: 14, name: 'Blueberry Banana', description: '', img_url: "https://i.imgur.com/xlV710v.png" },
-  { id: 16, name: 'Mint Banana', description: '', img_url: "https://i.imgur.com/EkdHoLM.png" },
+  { id: 11, name: 'Classic Banana', description: 'The classic raw taste of banana', img_url: "https://i.imgur.com/ycNLJ3E.png" },
+  { id: 12, name: 'Chocolate Banana', description: 'For monkeys who want more', img_url: "https://i.imgur.com/CICksMW.png" },
+  { id: 13, name: 'Mango Banana', description: "A monkey's favourite combo", img_url: "https://i.imgur.com/tJeyf3J.png" },
+  { id: 14, name: 'Strawberry Banana', description: 'For sweet healthy monkeys', img_url: "https://i.imgur.com/yRKMSqe.png" },
+  { id: 14, name: 'Blueberry Banana', description: 'For blue monkeys', img_url: "https://i.imgur.com/xlV710v.png" },
+  { id: 16, name: 'Mint Banana', description: 'For chilly monkeys', img_url: "https://i.imgur.com/EkdHoLM.png" },
 ];
